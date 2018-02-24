d70241245c845a3a758eed39996a083c8b443ad3
antala <aadam99@gmail.com>
2018-02-24 19:58:18 +0100

    Clean histogram2.cpp

--- src/_Histogram2.cpp
+++ src/_Histogram2.cpp
@@ -9,8 +9,12 @@
 #include "MatrixCollection.cpp"
 #include "MatrixGenerator.cpp"
 
+/*
+This program aims to create a histogram to get average and total number of integers in the upper triangle part of the considered matrices.
+*/
+
 int main() {
-  double steps = 0.11;
+  double steps = 0.07;
   double maxConsistency = 3.8;
   Ush maxIndex = (int)floor(maxConsistency / steps);
 
@@ -21,25 +25,12 @@ int main() {
   std::vector<unsigned int> bucketsTriang2(maxIndex);
   std::vector<unsigned int> bucketsTriang3(maxIndex);
 
-  std::vector<double> outputAll(maxIndex);
-  std::cout << "line2.1\n";
-  std::vector<double> outputTriang(maxIndex);
-  std::cout << "line2.2\n";
-  std::vector<double> outputTriang0(maxIndex);
-  std::cout << "line2.3\n";
-  std::vector<double> outputTriang1(maxIndex);
-  std::cout << "line2.4\n";
-  std::vector<double> outputTriang2(maxIndex);
-  std::cout << "line2.5\n";
-  std::vector<double> outputTriang3(maxIndex);
-  std::cout << "line3\n";
-
-  std::ofstream iss("../res/basicHistogram2.csv");
-
   MatrixCollection<4> m = MatrixCollection<4>::readFromFile(PATH_4_ALL);
   std::cout << "Read finished! The number of matrices is " << m.size() <<"\n";
+  std::cout << "Regularizing\n";
   m.regularize ();
 
+  std::cout << "Calculating upper triangles\n"
   std::vector<double> ratiosAll;
   std::vector<Ush> numberTriang;
   for (size_t i = 0; i < m.size(); i++) {
@@ -50,10 +41,7 @@ int main() {
     numberTriang.push_back(6 - m[i].countIndexOfUpperTriangle ());
   }
 
-  std::cout << "Consistencies calulcated.\n";
   std::cout << "Creating histogram...\n";
-
-  std::cout << "line1\n";
 
   if (ratiosAll.size() != numberTriang.size())
     throw "Sizes mismtach";
@@ -80,7 +68,6 @@ int main() {
                 bucketsTriang3.at(index) += 1;
             break;
             default:
-                std::cout << "OH MY GOD! Something really went wrong.\n";
                 throw "ERROR!";
             break;
         }
@@ -88,7 +75,14 @@ int main() {
     }
   }
 
-  std::cout << "line2\n";
+  std::cout << "Bucketing\n";
+
+  std::vector<double> outputAll(maxIndex);
+  std::vector<double> outputTriang(maxIndex);
+  std::vector<double> outputTriang0(maxIndex);
+  std::vector<double> outputTriang1(maxIndex);
+  std::vector<double> outputTriang2(maxIndex);
+  std::vector<double> outputTriang3(maxIndex);
 
   for (size_t i = 0; i < bucketsAll.size(); i++) {
     outputAll.at(i) = bucketsAll.at(i);
@@ -103,19 +97,16 @@ int main() {
     }
   }
 
-  std::cout << "line4\n";
+  std::cout << "Bucketing done, writing file\n";
 
-  std::cout << "Bucketing done, writing file...\n";
-
-  std::cout << "Program ok\n";
+  std::ofstream iss("../res/basicHistogram2.csv");
   //Header:
-  //I << "interval\t#all\t%eigen\t%spantree\t%cosine\t%common\t\n";
+  iss << "consistency\t#all\tavg upper\t6 upper\t5 upper\t4 upper\t3 upper\n";
   for (size_t i = 0; i < bucketsAll.size(); i++) {
     std::cout << i << " " << bucketsAll.size() << "\n";
     iss << i * steps << "\t" << outputAll.at(i) << "\t" <<
         outputTriang.at(i) << "\t" << outputTriang0.at(i) << "\t" << outputTriang1.at(i) << "\t" << outputTriang2.at(i) << "\t" << outputTriang3.at(i) << "\n";
   }
-  std::cout << "About to close the connection.\n";
   iss.close();
   std::cout << "Procedure finished.\n";
 
